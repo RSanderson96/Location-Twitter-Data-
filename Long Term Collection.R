d6f86ad2eeb5521907c7414ef277099616fae1dc
Rachael Sanderson (PGR) <b9054751@newcastle.ac.uk>
2021-05-21 10:25:04 +0100

    Cleaning code

--- Long Term Collection.R
+++ Long Term Collection.R
@@ -1,23 +1,22 @@
 #https://cran.r-project.org/web/packages/rtweet/vignettes/stream.html
+
+#1) Import packages - only rtweet needed at this stage
 library(rtweet)
 
+#2) Set Path
 Path = getwd()
 
-#1) Get twitter keys from seperate (private) R file
+#3) Get twitter keys from seperate (private) R file (see ReadMe introduction)
 source("Twitter_Keys.R")
 
-
-
+#4) Set parameters
 filename <- "LB_Tweets.json" #File to store tweets - this can be accessed in another R window during collection
-
 ## Stream location used to filter tweets
 Bounding_box <- c(-10.79,49.8,1.99,58.86) #location bounding box for where tweets can be collected from - this has been set to the UK
-
 ## Stream time in seconds so for one minute set timeout = 60
 ## For larger chunks of time, I recommend multiplying 60 by the number
 ## of desired minutes. This method scales up to hours as well
-## (x * 60 = x mins, x * 60 * 60 = x hours)
-## Stream for 30 minutes
+## (x * 60 = x mins, x * 60 * 60 = x hours
 
 Ten_Seconds = 10
 Five_Minutes = 60L * 5
@@ -26,8 +25,7 @@ One_Week = 60L * 60L * 24L* 7L
 Two_WeekS = 60L * 60L * 24L *7L * 2L
 Four_WeekS = 60L * 60L * 24L *7L * 4L
 
-## stream_tweets
-
+#5) stream_tweets
 stream_tweets(
   q = Bounding_box,
   parse = FALSE,
